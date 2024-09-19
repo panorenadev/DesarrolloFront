ccc7939f9884590d0cc1ed985a060a5dfcdff5d4
Paola Andrea Noreña Cardona <panoreaac@eafit.edu.co>
2024-09-18 23:02:33 -0500

    Add files via upload

---
+++ EjemplosJavaScript/Arreglos/script.js
@@ -0,0 +1,20 @@
+let computers = ["Desk", "Laptop", "Tablet PC"];
+ //console.log(computers.join(" | ")); 
+
+
+//computers.push("Smart Phone", 5); 
+//computers.unshift("Smart Phone", 5); 
+//computers.shift(); 
+//computers.pop();
+
+//computers.forEach(function(item) {
+//     item = `<li>${item}</li>`;
+//     console.log(item);
+// });
+
+// for(let i = 0; i < computers.length; i++) {
+//    console.log(computers[i]);
+//  }
+console.log(computers);
+
+//console.log(computers[0]);
